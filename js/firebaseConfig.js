ef564511da12a105425a0f8e5f003556d33f540d
Dizzy B High <dizzybhigh@gmail.com>
2025-12-06 14:48:10 +0100

    seperated database from main firebase config

---
+++ js/firebaseConfig.js
@@ -0,0 +1,12 @@
+// REPLACE THIS WITH YOUR FIREBASE CONFIG
+
+//Chip N Cheerful DB
+// HSRP BUSINESS - Chip N Cheerful Lunber
+const firebaseConfig = {
+    apiKey: "AIzaSyC9lipDKEux_PsPiPbhLcWFQej0xXjrjFI",
+    authDomain: "hsrp---chip-n-cheerful-lumber.firebaseapp.com",
+    projectId: "hsrp---chip-n-cheerful-lumber",
+    storageBucket: "hsrp---chip-n-cheerful-lumber.firebasestorage.app",
+    messagingSenderId: "982836124888",
+    appId: "1:982836124888:web:f3b75f76b4c68aaeba4bad"
+};
